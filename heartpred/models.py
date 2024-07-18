469af97cbf683ce79c47b65ff6465551ec81b7ab
Hussein <answereeee4@gmail.com>
2024-07-18 02:51:30 -0700

    013 - setting up the models

--- heartpred/models.py
+++ heartpred/models.py
@@ -1,2 +1,19 @@
 from django.db import models
 
+class heart(models.Model):
+    age = models.FloatField()
+    anaemia = models.IntegerField()
+    creatinine_phosphokinase = models.IntegerField()
+    ejection_fraction = models.IntegerField()
+    high_blood_pressure = models.IntegerField()
+    platelets = models.FloatField()
+    serum_creatinine = models.FloatField()
+    serum_sodium = models.IntegerField()
+    sex = models.IntegerField()
+    smoking = models.IntegerField()
+    time = models.IntegerField()
+    DEATH_EVENT = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.ceated

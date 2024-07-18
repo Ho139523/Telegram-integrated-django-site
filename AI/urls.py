3ee072ef2802c939863e9be35a15b5278f944821
Hussein <answereeee4@gmail.com>
2024-07-18 02:51:30 -0700

    013 - setting up the models

--- AI/urls.py
+++ AI/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('heartpred.urls')),
-    path('api/', include('myapi.urls'),
-]
+    path('api/', include('myapi.urls')),
+    ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

ab31ae801c7391750d067b6043893456d7f22854
Hussein <answereeee4@gmail.com>
2024-07-20 11:32:32 -0700

    015 - stablishing api view for creating

--- myapi/views.py
+++ myapi/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from heartpred.models import heart
+from .serializer import HeartSerializer
 
-# Create your views here.
+class HeartCreateAPIView(CreateAPIView):
+    query_set = heart.objects.all()
+    serializer_class = HeartSerializer

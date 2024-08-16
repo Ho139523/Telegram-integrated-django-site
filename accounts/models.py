24c13cadac96015ecd63fcf0975b2cd77212e645
Hussein <answereeee4@gmail.com>
2024-08-15 23:21:51 -0700

    change user model fields

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# class profile()
+
+class User(AbstractUser):
+    special_user=models.DateTimeField(default=timezone.now)
+    
+    def is_special_user(self):
+        if special_user > timezone.now():
+            return True
+        else:
+            return False

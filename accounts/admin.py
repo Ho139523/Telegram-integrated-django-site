acebfe2cd703f2d5aa3c535b91bbab2ec286eeee
Hussein <answereeee4@gmail.com>
2024-08-16 03:47:50 -0700

    adding special user to admin app

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+
+UserAdmin.list_display += ("is_special_user",)
+UserAdmin.fieldsets[2][1]["fields"]= (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "special_user",
+                    "groups",
+                    "user_permissions",
+                )
 admin.site.register(User, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,10 @@ class User(AbstractUser):
     special_user=models.DateTimeField(default=timezone.now)
     
     def is_special_user(self):
-        if special_user > timezone.now():
+        if self.special_user > timezone.now():
             return True
         else:
             return False
+    
+    is_special_user.boolean=True
+    is_special_user.short_description="Special User"

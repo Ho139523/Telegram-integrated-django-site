2a8437397804fb8b380d48aca09dacf21e7e437c
Hussein <answereeee4@gmail.com>
2024-09-09 17:57:21 -0700

    041 - signup fixed and billing template prepared

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import *
 from django.contrib.auth import views
 
@@ -7,10 +7,13 @@ urlpatterns = [
 
     path('login/', login.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
+    
     path('signup/', signup_user, name='signup'),
+    path('activate/<uidb64>/<token>/', activate, name='activate'),
+    
     path('dashboard/<slug:username>/profile', profile, name="profile"),
     path('panel/<slug:username>/dashboard', profile, name="dashboard"),
-    path('panel/<slug:username>/billing', profile, name="billing"),
+    path('panel/<slug:username>/billing', billing, name="billing"),
     path('panel/<slug:username>/notifications', profile, name="notifications"),
 
     path('panel/<slug:username>/password-change', ChangePassword.as_view(), name="Change Password"),

4b0ba0d34ccc26eff90aeef912a4a93ee1679c9d
Hussein <answereeee4@gmail.com>
2024-10-03 10:39:28 +0330

    056 - user address edit part6

--- accounts/form.py
+++ accounts/form.py
@@ -61,8 +61,8 @@ class ProfileUpdateForm(forms.ModelForm):
         
         widgets = {
                 'about_me': forms.TextInput(attrs={"placeholder": "Describe yourself and let others know you better", 'class': 'form-control d-none', 'id': 'aboutmeField'}),
-                'fname': forms.TextInput(attrs={"placeholder": "Type in your first name", 'class': 'form-control d-none', 'id': 'fnameField'}),
-                'lname': forms.TextInput(attrs={"placeholder": "Type in your last name", 'class': 'form-control d-none', 'id': 'lnameField'}),
+                'fname': forms.TextInput(attrs={"placeholder": "Type in your first name", 'class': 'form-control'}),
+                'lname': forms.TextInput(attrs={"placeholder": "Type in your last name", 'class': 'form-control'}),
                 'Phone': forms.TextInput(attrs={"placeholder": "Type in your phone number", 'class': 'form-control d-none', 'id': 'phoneField'}),
                 'birthday': forms.TextInput(attrs={"placeholder": "Type in your birthday date", 'class': 'form-control d-none', 'id': 'birthdayField'}),
                 # 'address': forms.TextInput(attrs={"placeholder": "Type in your address", 'class': 'form-control d-none', 'id': 'addressField'}),
@@ -75,9 +75,9 @@ class ProfileUpdateForm(forms.ModelForm):
         self.helper = FormHelper()
         self.helper.form_show_labels = False
         self.helper.layout = Layout(
-            Field('fname', css_class='form-control d-none', wrapper_class='col-6', id='fnameField'),
+            Field('fname', css_class='form-control', wrapper_class='col-6'),
             Field('about_me', css_class='form-control d-none', wrapper_class='col-6', id='aboutmeField'),
-            Field('lname', css_class='form-control d-none', wrapper_class='col-6', id='lnameField'),
+            Field('lname', css_class='form-control', wrapper_class='col-6'),
             Field('Phone', css_class='form-control d-none', wrapper_class='col-6', id='phoneField'),
             # Field('address', css_class='form-control d-none', wrapper_class='col-6', id='addressField'),
             Field('birthday', css_class='form-control d-none', wrapper_class='col-6'),

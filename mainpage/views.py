bed3977dc5422010efb54810aadf0aa92e091604
Hussein <answereeee4@gmail.com>
2024-10-06 10:05:19 +0330

    064 - mainpage articles

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from products.models import TutorialModel
+from products.models import TutorialModel, ArticleModel
 from accounts.models import User
 
 # Create your views here.
@@ -13,7 +13,8 @@ def home(request):
     
     context={
         "tutorials": TutorialModel.objects.filter(status=True),
-        "users": User.objects.filter(is_superuser=True)
+        "users": User.objects.filter(is_superuser=True),
+        "articles": ArticleModel.objects.filter(status=True),
     }
     
     return render(request, "mainpage/mainpage.html", context=context)

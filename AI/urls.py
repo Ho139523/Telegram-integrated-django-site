17b497a21711ee1d3554d4419060d9a8c665c002
Hussein <answereeee4@gmail.com>
2024-12-03 23:28:57 +0330

    update

--- AI/urls.py
+++ AI/urls.py
@@ -6,14 +6,14 @@ from utils.funcs.django_social_redirect import custom_complete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainpage.urls')),
+    path('', include('mainpage.urls')),  # OK if it needs the root path
     path('api/', include('myapi.urls')),
-    path('', include('cv.urls', namespace='cv')),
-    path('', include('heartpred.urls', namespace='heartpred')),
+    path('cv/', include('cv.urls', namespace='cv')),  # Use a distinct prefix
+    path('heartpred/', include('heartpred.urls', namespace='heartpred')),  # Use a distinct prefix
     path('accounts/', include('accounts.urls')),
     path('socials/', include('social_django.urls', namespace='social')),
     path('socials/complete/google-oauth2/', custom_complete, name="custom_complete"),
-    path('', include('telbot.urls', namespace='telbot')),
-    ]
+    path('telbot/', include('telbot.urls', namespace='telbot')),  # Prefix `telbot` instead of empty ''
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

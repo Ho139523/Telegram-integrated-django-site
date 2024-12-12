57ac0763fed29a568722b54f2705d04b20f7a91d
Hussein <answereeee4@gmail.com>
2024-12-13 03:27:35 +0330

    085 - making tel views.py bilingual

--- telbot/urls.py
+++ telbot/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from .views import TelegramBotWebhookView
 from myapi.views import CheckTelegramUserRegistrationView
 
-app_name='telbot'
+app_name = 'telbot'
+
 urlpatterns = [
     path('webhook/', TelegramBotWebhookView.as_view(), name='telegram_webhook'),
     
-    # Check telegram Registration
+    # check telegram registration
     path('api/check-registration/', CheckTelegramUserRegistrationView.as_view(), name='check-registration'),
 ]

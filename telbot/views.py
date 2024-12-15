615db1bf90a36b01074e43ab2be03021ab6db355
Hussein <answereeee4@gmail.com>
2024-12-15 22:24:24 +0330

    update

--- telbot/views.py
+++ telbot/views.py
@@ -125,19 +125,14 @@ def start(message):
     # Create keyboard for subscription check
     channel_markup = types.InlineKeyboardMarkup()
     current_site_markup = types.InlineKeyboardMarkup(row_width=1, resize_keyboard=True)
-    try:
-        current_site_button = types.InlineKeyboardButton(text='بازدید از سایت', url=f"{current_site}")
-        current_site_markup.add(current_site_button)
-    except Exception as e:
-        app.send_message(message.chat.id, f"error is : {e}")
-        
+    current_site_button = types.InlineKeyboardButton(text='بازدید از سایت', url=f"hello")
     check_subscription_button = types.InlineKeyboardButton(text='عضو شدم.', callback_data='check_subscription')
     channel_subscription_button = types.InlineKeyboardButton(text='در کانال ما عضو شوید...', url=f"https://t.me/{my_channels_without_atsign[0]}")
     group_subscription_button = types.InlineKeyboardButton(text="در گروه ما عضو شوید...", url=f"https://t.me/{my_channels_without_atsign[1]}")
     
     channel_markup.add(channel_subscription_button, group_subscription_button)
     channel_markup.add(check_subscription_button)
-    
+    current_site_markup.add(current_site_button)
 
     # Handle the response based on status code
     if response.status_code == 201:

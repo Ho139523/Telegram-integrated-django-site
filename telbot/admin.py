a2e13eab6098f09e848c190874e4f403060b0d74
Hussein <answereeee4@gmail.com>
2024-12-16 00:42:59 +0330

    update

--- telbot/admin.py
+++ telbot/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import telbotid
 
-admin.site.register(telbotid)
+
+class telbotidAdmin(admin.ModelAdmin):
+    list_display = ('tel_id', 'credit')  # Fields to show in the list view
+    search_fields = ('tel_id',)  # Enable search by these fields
+    ordering = ('credit',)  # Default ordering
+    list_editable = ('credit',)  # Fields editable directly in the list view
+
+admin.site.register(telbotid, telbotidAdmin)
 
 

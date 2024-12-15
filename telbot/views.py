1c0037a3416aeb0f3c90ef0172c986487598981b
Hussein <answereeee4@gmail.com>
2024-12-15 22:45:08 +0330

    update

--- telbot/views.py
+++ telbot/views.py
@@ -125,10 +125,10 @@ def start(message):
     # Create keyboard for subscription check
     channel_markup = types.InlineKeyboardMarkup()
     current_site_markup = types.InlineKeyboardMarkup(row_width=1, resize_keyboard=True)
-    try:
-        current_site_button = types.InlineKeyboardButton(text='بازدید از سایت', url=f"{current_site[0:current_site.find('/telbot')]}")
-    except Exception as e:
-        app.send_message(message.chat.id, f'the error is : {e}')
+    # try:
+        # current_site_button = types.InlineKeyboardButton(text='بازدید از سایت', url=f"{current_site[0:current_site.find('/telbot')]}")
+    # except Exception as e:
+        # app.send_message(message.chat.id, f'the error is : {e}')
     check_subscription_button = types.InlineKeyboardButton(text='عضو شدم.', callback_data='check_subscription')
     channel_subscription_button = types.InlineKeyboardButton(text='در کانال ما عضو شوید...', url=f"https://t.me/{my_channels_without_atsign[0]}")
     group_subscription_button = types.InlineKeyboardButton(text="در گروه ما عضو شوید...", url=f"https://t.me/{my_channels_without_atsign[1]}")

f36dac0ded69b156682b85a253c021487084e6f5
Hussein <answereeee4@gmail.com>
2024-12-17 22:47:02 +0330

    update

--- telbot/views.py
+++ telbot/views.py
@@ -375,8 +375,6 @@ def handle_message(message):
         # if text in ["ورزشی", "کت و شلوار", "زمستانه", "کفش و کتونی", "تابستانه", "خشکبار", "خوار و بار", "سوپر مارکت", "لپتاب", "گوشی"]:
             # options = ["پر فروش ترین ها", "گران ترین ها", "ارزان ترین ها", "پر تخفیف ها"]
             # send_menu(message, options, "products", retun_menue)
-        chat_id = message.chat.id
-        app.reply_to(message, f"Your Chat ID is: {chat_id}")
         app.send_message(message.chat.id, "دستور نامعتبر است. لطفاً یکی از گزینه‌های منو را انتخاب کنید.")
 
 

a8e08552c2fa0e0d0ece563d76a32171874bbf90
Hussein <answereeee4@gmail.com>
2024-12-17 15:52:58 +0330

    update

--- telbot/views.py
+++ telbot/views.py
@@ -93,7 +93,7 @@ def start(message):
         
         # Create keyboard for subscription check
         channel_markup = types.InlineKeyboardMarkup()
-        current_site_markup = types.InlineKeyboardMarkup(row_width=1, resize_keyboard=True)
+        current_site_markup = types.InlineKeyboardMarkup(row_width=1)
         current_site_button = types.InlineKeyboardButton(text='بازدید از سایت', url=f"{current_site}")
         check_subscription_button = types.InlineKeyboardButton(text='عضو شدم.', callback_data='check_subscription')
         channel_subscription_button = types.InlineKeyboardButton(text='در کانال ما عضو شوید...', url=f"https://t.me/{my_channels_without_atsign[0]}")

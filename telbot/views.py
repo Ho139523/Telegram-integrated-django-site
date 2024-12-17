1847389c67acce6f264339d0bdc3ba21604518f5
Hussein <answereeee4@gmail.com>
2024-12-18 00:44:02 +0330

    update

--- telbot/views.py
+++ telbot/views.py
@@ -354,7 +354,7 @@ def answer_text(message):
 
         try:
             user_message = texts[user]
-            app.send_message(chat_id=user, text=f"Your message:\n<i>{escape_special_characters(user_message)}</i>\n\nSupport answer:\n<b>{escape_special_characters(message.text)}</b>", parse_mode="HTML")
+            app.send_message(chat_id=user, text=f"پیام شما:\n\n<i>{escape_special_characters(user_message)}</i>\n\nپیام پشتیبان:\n\n<b>{escape_special_characters(message.text)}</b>", parse_mode="HTML")
             app.send_message(chat_id=message.chat.id, text="Your answer was sent!")
 
             del texts[user]
@@ -480,7 +480,7 @@ def answer(call):
         clean_text = BeautifulSoup(call.message.text, "html.parser").get_text()
         user = re.findall(pattern=pattern, string=clean_text)[0].split()[4]
         
-        app.send_message(chat_id=call.message.chat.id, text=f"Send your answer to <code>{user}</code>:", reply_markup=types.ForceReply(), parse_mode="HTML")
+        app.send_message(chat_id=call.message.chat.id, text=f"پاسختان را به کاربر با کد <code>{user}</code> ارسال کنید:", reply_markup=types.ForceReply(), parse_mode="HTML")
 
         app.set_state(user_id=call.from_user.id, state=Support.respond, chat_id=call.message.chat.id)
     

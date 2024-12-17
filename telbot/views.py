808f6596d51725b040d67b221e165b0e6d002b55
Hussein <answereeee4@gmail.com>
2024-12-17 14:30:32 +0330

    update

--- telbot/views.py
+++ telbot/views.py
@@ -183,12 +183,12 @@ def handle_message(message):
 
     # Products
     elif text in ["ورزشی", "کت و شلوار", "زمستانه", "کفش و کتونی", "تابستانه", "خشکبار", "خوار و بار", "سوپر مارکت", "لپتاب", "گوشی"]:
-        # if text in ["ورزشی", "کت و شلوار", "زمستانه", "کفش و کتونی", "تابستانه"]:
-            # user_menu_stack[chat_id].append('پوشاک')
-        # elif text in ["خشکبار", "خوار و بار", "سوپر مارکت"]:
-            # user_menu_stack[chat_id].append('خوراکی')
-        # elif text in ["لپتاب", "گوشی"]:
-            # user_menu_stack[chat_id].append('دیجیتال')
+        if text in ["ورزشی", "کت و شلوار", "زمستانه", "کفش و کتونی", "تابستانه"]:
+            user_menu_stack[chat_id].append('پوشاک')
+        elif text in ["خشکبار", "خوار و بار", "سوپر مارکت"]:
+            user_menu_stack[chat_id].append('خوراکی')
+        elif text in ["لپتاب", "گوشی"]:
+            user_menu_stack[chat_id].append('دیجیتال')
           # Save the subcategory name in the stack
         options = ["پر فروش ترین ها", "گران ترین ها", "ارزان ترین ها", "پر تخفیف ها"]
         send_menu(chat_id, options, user_menu_stack[chat_id][-1], retun_menue)

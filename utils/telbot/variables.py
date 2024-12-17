dd65985de0342f190966be1475e771c06f1961f9
Hussein <answereeee4@gmail.com>
2024-12-17 11:27:39 +0330

    update

--- utils/telbot/variables.py
+++ utils/telbot/variables.py
@@ -1,3 +1,3 @@
 # Define main menu
-main_menu = ["موجودی","hello", "helo", "خرید با کد کالا", "دسته بندی ها"]
+main_menu = ["موجودی","hello", "helo", 'hello', "خرید با کد کالا", "دسته بندی ها"]
 extra_buttons = ["بازدید سایت", "پیام به پشتیبان"]

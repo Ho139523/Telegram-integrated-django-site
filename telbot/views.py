273836a41a6cd9ad1212feec63a5d9fea2072e18
Hussein <answereeee4@gmail.com>
2024-12-21 00:34:39 +0330

    096 - changing layer 1 and layer 2 subcategory in telbot according to db

--- telbot/views.py
+++ telbot/views.py
@@ -277,14 +277,14 @@ def category_1(message):
         
         
 # Second layer category
-@app.message_handler(func=lambda message: str(message.text).lower() in [i.lower() for i in list(Category.objects.filter(parent__isnull=True).values_list('title', flat=True))])
-def category_2(message):
-    if subscription_offer(message):
-        layer_2 = list(Category.objects.filter(parent__title__iexact=message.text.lower()).values_list('title', flat=True))
-        try:
+try:
+    @app.message_handler(func=lambda message: message.text.lower() in [i.lower() for i in list(Category.objects.filter(parent__isnull=True).values_list('title', flat=True))])
+    def category_2(message):
+        if subscription_offer(message):
+            layer_2 = list(Category.objects.filter(parent__title__iexact=message.text.lower()).values_list('title', flat=True))
             send_menu(message, layer_2, "category_2", retun_menue)
-        except Exception as e:
-            app.send_message(chat_id, f"error is : {e}")
+except Exception as e:
+    print(f"{e}")
 
 
 # Products Handler

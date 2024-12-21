bc76db2e47ce01132491622e367c60b66f69ede7
Hussein <answereeee4@gmail.com>
2024-12-21 04:04:58 -0800

    "update"

--- telbot/views.py
+++ telbot/views.py
@@ -163,7 +163,7 @@ def start(message):
             try:
                 send_menu(message, main_menu, "main_menu", extra_buttons)
             except Exception as e:
-        app.send_message(message.chat.id, f"the error is: {e}")
+                app.send_message(message.chat.id, f"the error is: {e}")
         else:
             app.send_message(message.chat.id, f"the error is: ")
         

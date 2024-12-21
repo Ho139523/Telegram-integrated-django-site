de73ef4333eb970eb394bf4f738d068e930ab6a1
Hussein <answereeee4@gmail.com>
2024-12-21 21:08:47 +0330

    update

--- telbot/views.py
+++ telbot/views.py
@@ -200,6 +200,7 @@ def handle_back(message):
     if subscription_offer(message):
         try:
             session = user_sessions[message.chat.id]
+            app.send_message(message.chat.id, f"{session['current_menu']}")
             previous_category = Category.objects.get(title__iexact=session["current_menu"], status=True).get_parents()[1].title
             app.send_message(message.chat.id, previous_category)
         except Exception as e:

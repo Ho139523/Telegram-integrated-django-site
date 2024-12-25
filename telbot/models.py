58f9b87e6ca09200b2190f0487d913a49a9f3ba1
Hussein <answereeee4@gmail.com>
2024-12-25 20:50:45 +0330

    update

--- telbot/models.py
+++ telbot/models.py
@@ -3,9 +3,9 @@ from accounts.models import User
 from accounts.models import ProfileModel
 
 class telbotid(models.Model):
-    profile = models.OneToOneField(ProfileModel, on_delete=models.CASCADE, null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    tel_id = models.CharField(max_length=100, null=False, blank=False)
+    profile = models.OneToOneField(ProfileModel, unique=True, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, unique=True, on_delete=models.SET_NULL, null=True, blank=True)
+    tel_id = models.CharField(max_length=100, unique=True, null=False, blank=False)
     credit = models.DecimalField(max_digits=10, decimal_places=2, default=0, blank=True)
     
     def __str__(self):

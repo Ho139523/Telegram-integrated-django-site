117e59826c42459f496e03276187fca4f9b729e0
Hussein <answereeee4@gmail.com>
2024-12-25 20:59:56 +0330

    update

--- myapi/views.py
+++ myapi/views.py
@@ -45,8 +45,9 @@ class CheckTelegramUserRegistrationView(APIView):
         profile_exists = list(ProfileModel.objects.filter(telegram=tel_id).values_list('telegram', flat=True))
         
         if tel_id in profile_exists:
-            new_telbotid = telbotid.objects.create(user=None, tel_id=tel_id)
-            new_telbotid.save()
+            if tel_id not in telbotid_exists:
+                new_telbotid = telbotid.objects.create(user=None, tel_id=tel_id)
+                new_telbotid.save()
             return Response({
                 "message": f"{tel_id} عزیز شما قبلا در ربات ثبت‌نام کرده‌اید."
             }, status=status.HTTP_200_OK)

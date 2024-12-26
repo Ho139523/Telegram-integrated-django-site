534aeefb2b366f46174b89a3bd4ac4e2735663f5
Hussein <answereeee4@gmail.com>
2024-12-26 05:08:37 +0330

    100 - create account added.

--- telbot/urls.py
+++ telbot/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import TelegramBotWebhookView
-from myapi.views import CheckTelegramUserRegistrationView
+from myapi.views import CheckTelegramUserRegistrationView, telegram_activation_redirect
 
 app_name = 'telbot'
 

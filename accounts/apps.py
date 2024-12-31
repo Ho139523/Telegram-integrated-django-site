6b3090c0576596965a3b5f9dc8df179bc64ee112
Hussein <answereeee4@gmail.com>
2025-01-01 02:38:10 +0330

    update

--- accounts/apps.py
+++ accounts/apps.py
@@ -3,8 +3,3 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-
-    def ready(self):
-        import django.db.models.signals  # اطمینان از لود شدن سیگنال‌های اصلی
-        from django.utils.module_loading import autodiscover_modules
-        autodiscover_modules('signals')
--- telbot/apps.py
+++ telbot/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class TelbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'telbot'
-    
-    
-    def ready(self):
-        from . import signals  # بارگذاری سیگنال‌ها

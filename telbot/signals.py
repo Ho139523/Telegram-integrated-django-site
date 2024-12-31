749d502e7e90d7bdb9c6e52c80f8cf547964bf38
Hussein <answereeee4@gmail.com>
2024-12-31 20:47:47 +0330

    update

---
+++ telbot/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=telbotid)
+def sync_profile_credit_from_telbot(sender, instance, **kwargs):
+  from accounts.models import ProfileModel
+  from .models import telbotid
+  if hasattr(instance, '_syncing'):
+      return
+  instance._syncing = True
+  profile = instance.profile
+  if profile and profile.credit != instance.credit:
+      profile.credit = instance.credit
+      profile._syncing = True
+      profile.save()
+  del instance._syncing

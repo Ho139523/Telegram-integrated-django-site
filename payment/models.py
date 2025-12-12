4d388fdf252737668361704064023e053cb058a4
Hussein <answereeee4@gmail.com>
2025-12-12 16:30:40 +0330

    150 - promptvideos mechanism 2 done. django json for hidden video

--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from products.models import Product, Store  # Ensure you have a Product model
+from products.models import Product, Store
 import uuid
 from accounts.models import ProfileModel
 from django.core.validators import MinValueValidator
--- telbot/models.py
+++ telbot/models.py
@@ -34,22 +34,20 @@ class CachedMedia(models.Model):
         ('document', 'document'),
     )
 
-    profile = models.ForeignKey("accounts.ProfileModel", on_delete=models.CASCADE, related_name="Cachedmedia")
     media_type = models.CharField(max_length=20, choices=MEDIA_TYPE_CHOICES, default='video')
-    video_path = models.CharField(max_length=1024, unique=True)   # path on disk (برای مرجع)
-    file_id = models.CharField(max_length=512, blank=True, null=True)  # telegram file_id
+    video_path = models.CharField(max_length=1024, unique=True)
+    file_id = models.CharField(max_length=512, blank=True, null=True)
     channel_message_id = models.BigIntegerField(blank=True, null=True)
-    channel_id = models.CharField(max_length=255, blank=True, null=True)  # e.g. @your_channel
+    channel_id = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.BooleanField(default=True, null=False)
-    
-    def path(self, video_path):
-        return video_path.split("/")[-1]
-    
+
+    def path(self):
+        return self.video_path.split("/")[-1]
+
     @property
     def short_name(self):
-        return f"{self.profile.lang}-{self.path(self.video_path)}"
+        return f"{self.path()}"
 
     def __str__(self):
-        return f"{self.media_type} - {self.path(self.video_path)}"
+        return f"{self.media_type} - {self.path()}"
 
